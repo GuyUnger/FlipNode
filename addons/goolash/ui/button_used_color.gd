2a942597084b83aae8f921629cafd99aabc13f6f
Guy Unger <guyunger@gmail.com>
2023-12-03 03:57:11 +0100

    default colors

--- addons/goolash/ui/button_used_color.gd
+++ addons/goolash/ui/button_used_color.gd
@@ -1,13 +1,10 @@
 @tool
 extends Button
 
-var color: Color
-
 func set_color(color: Color):
-	self.color = color
 	modulate = color
 
 
 func _on_pressed():
-	GoolashEditor.editor.current_color = color
+	GoolashEditor.editor.current_color = modulate
 	GoolashEditor.hud._update_color_picker_color()
